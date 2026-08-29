b73618773d10b46564fea4006597d829e9b8216a
chitranshu07-coder <kumar.chitranshu007@gmail.com>
2026-08-29 18:29:03 +0530

    Time: 1430 ms (97.57%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,15 @@
+SELECT COUNT(*) AS accounts_count, 'Low Salary' AS category
+FROM Accounts
+WHERE income < 20000
+
+UNION ALL
+
+SELECT COUNT(*) AS accounts_count, 'Average Salary' AS category
+FROM Accounts
+WHERE income >= 20000 AND income <= 50000
+
+UNION ALL
+
+SELECT COUNT(*) AS accounts_count, 'High Salary' AS category
+FROM Accounts
+WHERE income > 50000;
