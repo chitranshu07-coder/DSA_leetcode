6c2b399500e519a33be9d11c013764abe15aedea
chitranshu07-coder <kumar.chitranshu007@gmail.com>
2026-08-30 03:58:21 +0530

    Time: 307 ms (86.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,9 @@
+select id,count(*)as num
+from
+(select requester_id as id from RequestAccepted 
+union all
+select accepter_id as id from RequestAccepted )
+as friend_count
+group by id
+order by num desc 
+limit 1
