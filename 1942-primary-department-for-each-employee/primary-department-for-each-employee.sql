486c483645a1861d828c64fe6f75bef12462281a
chitranshu07-coder <kumar.chitranshu007@gmail.com>
2026-08-29 09:18:38 +0530

    Time: 561 ms (77.72%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -2,7 +2,7 @@
 select employee_id,  department_id
 from Employee
 where primary_flag='Y'
-Union ALL
+Union 
 select employee_id,  department_id
 from Employee
 group by employee_id having count(*)=1
