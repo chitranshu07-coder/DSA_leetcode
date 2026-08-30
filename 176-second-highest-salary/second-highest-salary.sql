111963a71ee6920e8ec00af889788e32b903f862
chitranshu07-coder <kumar.chitranshu007@gmail.com>
2026-08-30 06:36:27 +0530

    Time: 314 ms (40.09%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,7 +1,10 @@
-SELECT MAX(temp.SecondHighestSalary) AS SecondHighestSalary
-FROM (
-    SELECT Salary AS SecondHighestSalary,
-           DENSE_RANK() OVER (ORDER BY Salary DESC) AS rnk
-    FROM Employee
-) AS temp
-WHERE rnk = 2;
+-- SELECT MAX(temp.SecondHighestSalary) AS SecondHighestSalary
+-- FROM (
+--     SELECT Salary AS SecondHighestSalary,
+--            DENSE_RANK() OVER (ORDER BY Salary DESC) AS rnk
+--     FROM Employee
+-- ) AS temp
+-- WHERE rnk = 2;
+select max(e1.salary) as SecondHighestSalary
+from Employee e1 join Employee e2 
+on e1.salary<e2.salary
